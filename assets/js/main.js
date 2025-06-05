7ecc08109be8cfa71cc2a11b023f0d298fb8995a
Bayarbileg Bayarsaikhan <bbayarbileg@gmail.com>
2025-06-06 00:10:48 +0200

    added favicon and fixed auto theme switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,5 +64,8 @@ document.addEventListener("DOMContentLoaded", () => {
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
+  return null;
 }
